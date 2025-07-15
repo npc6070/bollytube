5cf20e16360aa5822dee073ad1bfcb8a2289ce7b
npc6070 <npc6070@gmail.com>
2025-07-15 18:17:16 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
+
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TrendingVideos from './TrendingVideos';
 import SearchBar from './SearchBar';
 import VideoDetail from './VideoDetail';
+import Helmet from 'react-helmet';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
+          <h1 className="main-title">India’s Top Trending Videos</h1>
           <h1>YouTube Bollywood</h1>
           <SearchBar className="search-bar" />
         </header>
+        <Helmet>
+          <title>India’s Top Trending Videos</title>
+        </Helmet>
         <main>
           <Routes>
             <Route path="/" element={<TrendingVideos />} />
